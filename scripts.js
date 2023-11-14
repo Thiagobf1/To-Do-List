63fe177a80052e396f4d6b4765a577b5b46148de
Thiagobf1 <thiago.busifeijo@gmail.com>
2023-11-14 11:53:16 -0300

    Start Elements Selections

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Seleção Elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+
+
+
+// Funções
+
+
+//Eventos
